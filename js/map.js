2faf0ee13d4922ebbe23e299f4cf411d4fe77707
Lia5 <Lia_na5@mail.ru>
2017-11-20 02:07:02 +0200

    page portfolio-list

--- js/map.js
+++ js/map.js
@@ -18,26 +18,10 @@ function initMap() {
 
     map.scrollwheel=true;
   map.setOptions({ mapTypeControl:true});
- 
- /*Один маркер*/ 
- /*var marker = new google.maps.Marker({
-	position: {lat: 46.967046, lng: 32.000093},
-	map: map,
-	title: 'Legalizuem.ru',
-    /*Для svg*/
-    /*icon: {
-		url: "images/markers/svg/Arrow_3.svg",
-		scaledSize: new google.maps.Size(64, 64)
-    }*/
-    /*icon: {
-      url: window.devicePixelRatio > 1 ? "img/marker96.png" : "img/marker48.png",
-      scaledSize: new google.maps.Size(48, 48)
-    }
-});*/
 
   var locations = [
 	{
-		title: 'NobelDent',
+		title: 'reland',
 		position: {lat: 55.164878, lng: 37.465146},
 		icon: {
 			url: "img/marker.png",
@@ -54,10 +38,74 @@ locations.forEach( function( element ) {
 		});
 	});	
 }
-
- 
-// Ждем полной загрузки страницы, после этого запускаем initMap()
 google.maps.event.addDomListener(window, "load", initMap);
 
 
+
+
+
+// initMapHeader() - функция инициализации карты
+
+
+
+function initMapHeader() {
+  var centerLatLng = new google.maps.LatLng(55.771621, 37.672994);
+  var mapOptions = {
+      center: centerLatLng, 
+      zoom: 13,               // Зум по умолчанию. Возможные значения от 0 до 21
+      navigationControlOptions: {
+        style: google.maps.NavigationControlStyle.SMALL
+      },
+       mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("header-map"), mapOptions);
+
+  map.scrollwheel=true;
+map.setOptions({ mapTypeControl:true});
+
+var locations = [
+{
+  title: 'reland',
+  position: {lat: 55.775635, lng: 37.671331},
+  icon: {
+    url: "img/marker-tree.png",
+    scaledSize: new google.maps.Size(42, 57)
+  }
+},
+{
+  title: 'reland',
+  position: {lat: 55.770488, lng: 37.677904},
+  icon: {
+    url: "img/marker-tree.png",
+    scaledSize: new google.maps.Size(42, 57)
+  }
+},
+{
+  title: 'reland',
+  position: {lat: 55.765995, lng: 37.671454},
+  icon: {
+    url: "img/marker-house.png",
+    scaledSize: new google.maps.Size(36, 36)
+  }
+}, 
+{
+  title: 'reland',
+  position: {lat: 55.780708, lng: 37.632551},
+  icon: {
+    url: "img/marker-institute.png",
+    scaledSize: new google.maps.Size(36, 36)
+  }
+}					
+];
+locations.forEach( function( element ) {
+var marker = new google.maps.Marker({
+    position: element.position,
+    map: map,
+    title: element.title,
+    icon: element.icon,
+  });
+});	
+}
+// Ждем полной загрузки страницы, после этого запускаем initMap()
+google.maps.event.addDomListener(window, "load", initMapHeader);
 
