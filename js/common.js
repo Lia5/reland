c81a6fb77d68ce76751445c056bd5a0ef5ef922b
Lia5 <Lia_na5@mail.ru>
2017-12-07 16:53:30 +0200

    last

---
+++ js/common.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:0,
+  nav: false,
+  dots: false,
+  autoWidth:true,
+  autoplay:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:1
+      },
+      1000:{
+          items:1
+      },
+      1200: {
+          items:1
+      }
+  }
+})
--- js/script.js
+++ js/script.js
@@ -54,3 +54,134 @@ if(window.matchMedia('(min-width: 750px)').matches) {
 
 
 });
+
+
+
+if(jQuery('#portfolio__project__right-menu').length) {
+
+  
+(function(){  // анонимная функция (function(){ })(), чтобы переменные "a" и "b" не стали глобальными
+var a = document.querySelector('#portfolio__project__right-menu'), b = null;  // селектор блока, который нужно закрепить
+window.addEventListener('scroll', Ascroll, false);
+document.body.addEventListener('scroll', Ascroll, false);  // если у html и body высота равна 100%
+function Ascroll() {
+  if (b == null) {  // добавить потомка-обёртку, чтобы убрать зависимость с соседями
+    var Sa = getComputedStyle(a, ''), s = '';
+    for (var i = 0; i < Sa.length; i++) {  // перечислить стили CSS, которые нужно скопировать с родителя
+      if (Sa[i].indexOf('overflow') == 0 || Sa[i].indexOf('padding') == 0 || Sa[i].indexOf('border') == 0 || Sa[i].indexOf('outline') == 0 || Sa[i].indexOf('box-shadow') == 0 || Sa[i].indexOf('background') == 0) {
+        s += Sa[i] + ': ' +Sa.getPropertyValue(Sa[i]) + '; '
+      }
+    }
+    b = document.createElement('div');  // создать потомка
+    b.style.cssText = s + ' box-sizing: border-box; width: ' + a.offsetWidth + 'px;';
+    a.insertBefore(b, a.firstChild);  // поместить потомка в цепляющийся блок первым
+    var l = a.childNodes.length;
+    for (var i = 1; i < l; i++) {  // переместить во вновь созданного потомка всех остальных потомков (итого: создан потомок-обёртка, внутри которого по прежнему работают скрипты)
+      b.appendChild(a.childNodes[1]);
+    }
+    a.style.height = b.getBoundingClientRect().height + 'px';  // если под скользящим элементом есть другие блоки, можно своё значение
+    a.style.padding = '0';
+    a.style.border = '0';  // если элементу присвоен padding или border
+  }
+  if (a.getBoundingClientRect().top <= 0) { // elem.getBoundingClientRect() возвращает в px координаты элемента относительно верхнего левого угла области просмотра окна браузера
+    b.className = 'sticky';
+  } else {
+    b.className = '';
+  }
+  window.addEventListener('resize', function() {
+    a.children[0].style.width = getComputedStyle(a, '').width
+  }, false);  // если изменить размер окна браузера, измениться ширина элемента
+}
+})()
+
+}
+
+
+
+if(jQuery('#text_project__right-menu').length) {
+
+  
+(function(){  // анонимная функция (function(){ })(), чтобы переменные "a" и "b" не стали глобальными
+var a = document.querySelector('#text_project__right-menu'), b = null;  // селектор блока, который нужно закрепить
+window.addEventListener('scroll', Ascroll, false);
+document.body.addEventListener('scroll', Ascroll, false);  // если у html и body высота равна 100%
+function Ascroll() {
+  if (b == null) {  // добавить потомка-обёртку, чтобы убрать зависимость с соседями
+    var Sa = getComputedStyle(a, ''), s = '';
+    for (var i = 0; i < Sa.length; i++) {  // перечислить стили CSS, которые нужно скопировать с родителя
+      if (Sa[i].indexOf('overflow') == 0 || Sa[i].indexOf('padding') == 0 || Sa[i].indexOf('border') == 0 || Sa[i].indexOf('outline') == 0 || Sa[i].indexOf('box-shadow') == 0 || Sa[i].indexOf('background') == 0) {
+        s += Sa[i] + ': ' +Sa.getPropertyValue(Sa[i]) + '; '
+      }
+    }
+    b = document.createElement('div');  // создать потомка
+    b.style.cssText = s + ' box-sizing: border-box; width: ' + a.offsetWidth + 'px;';
+    a.insertBefore(b, a.firstChild);  // поместить потомка в цепляющийся блок первым
+    var l = a.childNodes.length;
+    for (var i = 1; i < l; i++) {  // переместить во вновь созданного потомка всех остальных потомков (итого: создан потомок-обёртка, внутри которого по прежнему работают скрипты)
+      b.appendChild(a.childNodes[1]);
+    }
+    a.style.height = b.getBoundingClientRect().height + 'px';  // если под скользящим элементом есть другие блоки, можно своё значение
+    a.style.padding = '0';
+    a.style.border = '0';  // если элементу присвоен padding или border
+  }
+  if (a.getBoundingClientRect().top <= 0) { // elem.getBoundingClientRect() возвращает в px координаты элемента относительно верхнего левого угла области просмотра окна браузера
+    b.className = 'sticky';
+  } else {
+    b.className = '';
+  }
+  window.addEventListener('resize', function() {
+    a.children[0].style.width = getComputedStyle(a, '').width
+  }, false);  // если изменить размер окна браузера, измениться ширина элемента
+}
+})()
+
+}
+
+
+
+
+
+if(jQuery('#popup__send-mail-small').length) {
+(function(){
+  var a = document.querySelector('#popup__send-mail-small'), b = null, P = 0;  // если ноль заменить на число, то блок будет прилипать до того, как верхний край окна браузера дойдёт до верхнего края элемента. Может быть отрицательным числом
+  window.addEventListener('scroll', Ascroll, false);
+  document.body.addEventListener('scroll', Ascroll, false);
+  function Ascroll() {
+    if (b == null) {
+      var Sa = getComputedStyle(a, ''), s = '';
+      for (var i = 0; i < Sa.length; i++) {
+        if (Sa[i].indexOf('overflow') == 0 || Sa[i].indexOf('padding') == 0 || Sa[i].indexOf('border') == 0 || Sa[i].indexOf('outline') == 0 || Sa[i].indexOf('box-shadow') == 0 || Sa[i].indexOf('background') == 0) {
+          s += Sa[i] + ': ' +Sa.getPropertyValue(Sa[i]) + '; '
+        }
+      }
+      b = document.createElement('div');
+      b.style.cssText = s + ' box-sizing: border-box; width: ' + a.offsetWidth + 'px;';
+      a.insertBefore(b, a.firstChild);
+      var l = a.childNodes.length;
+      for (var i = 1; i < l; i++) {
+        b.appendChild(a.childNodes[1]);
+      }
+      a.style.height = b.getBoundingClientRect().height + 'px';
+      a.style.padding = '0';
+      a.style.border = '0';
+    }
+    var Ra = a.getBoundingClientRect(),
+        R = Math.round(Ra.top + b.getBoundingClientRect().height - document.querySelector('#popup__send-mail').getBoundingClientRect().top + 200);  // селектор блока, при достижении верхнего края которого нужно открепить прилипающий элемент;  Math.round() только для IE; если ноль заменить на число, то блок будет прилипать до того, как нижний край элемента дойдёт до футера
+    if ((Ra.top - P) <= 0) {
+      if ((Ra.top - P) <= R) {
+        b.className = 'stop';
+        b.style.top = - R +'px';
+      } else {
+        b.className = 'sticky';
+        b.style.top = P + 'px';
+      }
+    } else {
+      b.className = '';
+      b.style.top = '';
+    }
+    window.addEventListener('resize', function() {
+      a.children[0].style.width = getComputedStyle(a, '').width
+    }, false);
+  }
+  })()
+}
